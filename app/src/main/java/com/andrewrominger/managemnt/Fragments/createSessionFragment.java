dcf85e290090dc7788267468e3d0a6b34c82cd94
WesBanghart <Wbang98@gmail.com>
2016-11-10 11:18:53 -0800

    added the studibudiactivity as well as several fragments

---
+++ app/src/main/java/com/andrewrominger/managemnt/Fragments/createSessionFragment.java
@@ -0,0 +1,10 @@
+package com.andrewrominger.managemnt.Fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by wesleybanghart on 11/5/16.
+ */
+
+public class createSessionFragment extends Fragment {
+}
---
+++ app/src/main/java/com/andrewrominger/managemnt/Fragments/sessionView.java
@@ -0,0 +1,10 @@
+package com.andrewrominger.managemnt.Fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by wesleybanghart on 11/4/16.
+ */
+
+public class sessionView extends Fragment {
+}
